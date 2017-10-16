e01dcb90ee0102dc5334f70281c78127db42c827
Elizarova Anastasiya <elizarova.at@gmail.com>
2017-10-16 14:30:27 +0300

    fix predicat

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,24 +5,24 @@ const description = 'Answer "yes" if number is prime otherwise answer "no".';
 
 const isPrime = (num) => {
   if (num < 2) {
-    return 'no';
+    return false;
   }
 
   let i = 2;
   while (i <= num / 2) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
     i += 1;
   }
-  return 'yes';
+  return true;
 };
 
 const gameData = () => {
   const randomNum = randomNumber(100, 0);
-  const question = (`Question: ${randomNum}`);
+  const question = `Question: ${randomNum}`;
 
-  const correctAnswer = isPrime(randomNum).toString();
+  const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
